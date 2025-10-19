9f164717e7178691828fb6e786d4a98184179381
ahmed <ahmedelibriki75@gmail.com>
2025-10-19 15:35:11 +0100

    Add Service EUM options to take.sql

--- take.sql
+++ take.sql
@@ -4,5 +4,6 @@ create table clients(
   Nom_et_prenom varchar(50) not null,
   Numéro_de_téléphone int not null,
   Email varchar(250) not null,
+  Service EUM ('Peinture automobile' , 'Peinture autobus' , 'Reparer les degats' , 'Consultation'),
   Message varchar(250)
   );
