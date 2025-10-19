791e53cd38124fab287e1e129a4d8994457e6efd
ahmed <ahmedelibriki75@gmail.com>
2025-10-19 15:31:59 +0100

    Create Contacts database and clients table

--- take.sql
+++ take.sql
@@ -1,1 +1,8 @@
-
+create database Contacts;
+create table clients(
+  Id int AUTO_INCREMENT primary key ,
+  Nom_et_prenom varchar(50) not null,
+  Numéro_de_téléphone int not null,
+  Email varchar(250) not null,
+  Message varchar(250)
+  );
